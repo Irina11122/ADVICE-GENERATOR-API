e16d1e967518aca0286820d73d1aadd00fca8742
unknown <irina.bojkovska.ib@gmail.com>
2025-08-07 21:07:06 +0200

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,64 @@
-import useSWR from "swr"
+import { useEffect, useState } from "react";
 import { AdviceId, Card } from "./components/Card";
+
 function App() {
- const fetcher = (url) => fetch(url).then((res) => res.json())
+  const [advice, setAdvice] = useState("");
+  const [adviceId, setAdviceId] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
+  const getAdvice = async () => {
+    setLoading(true);
+    setError(false);
+    try {
+      const res = await fetch(
+        `https://api.adviceslip.com/advice?timestamp=${Date.now()}`
+      );
+      const data = await res.json();
+      setAdvice(data.slip.advice);
+      setAdviceId(data.slip.id);
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+    setLoading(false);
+  };
 
- const {data,error,isLoading} = useSWR('https://api.adviceslip.com/advice', fetcher);
+  useEffect(() => {
+    getAdvice();
+  }, []);
 
- if(error) return <div> Error </div>
- if(isLoading) return <div> Loading... </div>
   return (
-    <div className="container"> 
-    <Card>
-      <AdviceId>
-         ADVICE  #{data?.slip?.id} 
-      </AdviceId>
-    
-      <h1> {`"${data?.slip?.advice}"`} </h1>    
-      <div>
-      <img style={{width: '100%'}} src="./public/pattern-divider-desktop.svg" alt="" />
+    <div className="container">
+      <Card>
+        {error && <div>Error fetching advice</div>}
+        {loading ? (
+          <div className="loader">Loading...</div>
+        ) : (
+          <>
+            <AdviceId>ADVICE #{adviceId}</AdviceId>
+            <h1>{`"${advice}"`}</h1>
+            <div>
+              <img
+                style={{ width: "100%" }}
+                src="/pattern-divider-desktop.svg"
+                alt="divider"
+              />
+            </div>
+          </>
+        )}
+      </Card>
+
+      <div className="dice-icon">
+        <img
+          onClick={getAdvice}
+          className="dice"
+          src="/icon-dice.svg"
+          alt="dice"
+        />
+      </div>
     </div>
-    </Card>
-    <div className="dice-icon" >
-        <img src="./public/icon-dice.svg" alt="" />
-    </div>
-    
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
